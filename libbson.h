372ccd380d1d27c4ee360596248a61469c1765b9
Mike Friedman <mike.friedman@10gen.com>
2012-09-13 09:03:09 -0400

    type constants for header

---
+++ libbson.h
@@ -0,0 +1,23 @@
+/* libbson headers */
+
+const int BSON_DOUBLE            = 0x01;
+const int BSON_STRING            = 0x02;
+const int BSON_DOCUMENT          = 0x03;
+const int BSON_ARRAY             = 0x04;
+const int BSON_BINRARY           = 0x05;
+const int BSON_UNDEFINED         = 0x06;
+const int BSON_OBJECT_ID         = 0x07;
+const int BSON_BOOLEAN           = 0x08;
+const int BSON_DATETIME          = 0x09;
+const int BSON_NULL              = 0x0A;
+const int BSON_REGEXP            = 0x0B;
+const int BSON_DB_POINTER        = 0x0C;
+const int BSON_JAVASCRIPT        = 0x0D;
+const int BSON_SYMBOL            = 0x0E;
+const int BSON_JAVASCRIPT_SCOPE  = 0x0F;
+const int BSON_INT32             = 0x10;
+const int BSON_TIMESTAMP         = 0x11;
+const int BSON_INT64             = 0x12;
+const int BSON_MINIMUM           = 0xFF;
+const int BSON_MAXIMUM           = 0x7F;
+
